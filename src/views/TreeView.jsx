3ba2f4ab342805685a896dabae3a5ed3a7607150
CraigBit <listokbymagi@gmail.com>
2022-09-06 03:16:12 +0700

    tree change

--- src/views/TreeView.jsx
+++ src/views/TreeView.jsx
@@ -7,14 +7,14 @@ import 'rc-tree/assets/index.css';
 
 export const TreeView = () => {
   const [tree, setTree] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   const [fetchTree, isTreeLoading, treeError] = useFetching(async () => {
     const response = await ItemService.getAll();
-    const result = [];
-    for (let i = 0; i < response.length; i += 100) {
-      result.push(response.slice(i, i + 100));
-    }
-    setTree(result);
+    const categoriesSet = new Set(response.map((item) => item.category));
+    const categoriesArray = Array.from(categoriesSet);
+    setTree(response);
+    setCategories(categoriesArray);
   });
 
   useEffect(() => {
@@ -40,56 +40,53 @@ export const TreeView = () => {
           expandAction='click'
           className='m-5'>
           <TreeNode title='Root' key='0-0-0-0-0-0'>
-            {tree.map((item, index) => {
-              let title;
-              if (index < 6) {
-                title = `[${index * 100}-${index * 100 + 99}]`;
-              } else {
-                title = `[${index * 100}-${index * 100 + 59}]`;
-              }
+            {categories.map((category, index) => {
+              let title = category;
               return (
                 <TreeNode title={title} key={index}>
-                  {item.map((node, index) => {
-                    const size = node.filesize;
-                    const category = node.category;
-                    const date = new Date(node.timestamp).toLocaleDateString(
-                      'ru-RU'
-                    );
-                    const path = `http://contest.elecard.ru/frontend_data/${node.image}`;
-                    const img = (
-                      <a href={path} target='_blank' rel='noreferrer'>
-                        <img src={path} alt='not found' style={imgStyle} />
-                      </a>
-                    );
-                    return (
-                      <TreeNode title={`${index}`} key={node.timestamp}>
-                        <TreeNode
-                          title={img}
-                          key={`${
-                            Date.now() + Math.random() * (99999 - 10) + 10
-                          }`}
-                        />
-                        <TreeNode
-                          title={size}
-                          key={`${
-                            Date.now() + Math.random() * (99999 - 10) + 10
-                          }`}
-                        />
-                        <TreeNode
-                          title={date}
-                          key={`${
-                            Date.now() + Math.random() * (99999 - 10) + 10
-                          }`}
-                        />
-                        <TreeNode
-                          title={category}
-                          key={`${
-                            Date.now() + Math.random() * (99999 - 10) + 10
-                          }`}
-                        />
-                      </TreeNode>
-                    );
-                  })}
+                  {tree
+                    .filter((item) => item.category === category)
+                    .map((node, index) => {
+                      const size = node.filesize;
+                      const category = node.category;
+                      const date = new Date(node.timestamp).toLocaleDateString(
+                        'ru-RU'
+                      );
+                      const path = `http://contest.elecard.ru/frontend_data/${node.image}`;
+                      const img = (
+                        <a href={path} target='_blank' rel='noreferrer'>
+                          <img src={path} alt='not found' style={imgStyle} />
+                        </a>
+                      );
+                      return (
+                        <TreeNode title={`${index}`} key={node.timestamp}>
+                          <TreeNode
+                            title={img}
+                            key={`${
+                              Date.now() + Math.random() * (99999 - 10) + 10
+                            }`}
+                          />
+                          <TreeNode
+                            title={size}
+                            key={`${
+                              Date.now() + Math.random() * (99999 - 10) + 10
+                            }`}
+                          />
+                          <TreeNode
+                            title={date}
+                            key={`${
+                              Date.now() + Math.random() * (99999 - 10) + 10
+                            }`}
+                          />
+                          <TreeNode
+                            title={category}
+                            key={`${
+                              Date.now() + Math.random() * (99999 - 10) + 10
+                            }`}
+                          />
+                        </TreeNode>
+                      );
+                    })}
                 </TreeNode>
               );
             })}
